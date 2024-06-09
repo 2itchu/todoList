c6ca401f4a5512ad83d863ceb51e902de6c80d1d
itchu <2itchu.pinstar@gmail.com>
2024-06-09 14:11:44 +0900

    useReducer 사용해보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react'
+import {useReducer, useRef, useState} from 'react'
 
 import './App.css'
 import Header from "./components/Header";
@@ -25,34 +25,63 @@ const mockData = [
         date : new Date().getTime(),
     }
 ]
+
+function reducer(state, action){
+    switch(action.type){
+        case 'CREATE' : return [action.data , ...state];
+        case 'UPDATE' :
+            return state.map((item)=>item.id===action.targetId? {...item, isDone : !item.isDone} : item);
+        case "DELETE" :
+            return state.filter((todo)=>todo.id !== action.targetId)
+        default : return state;
+    }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
-  const idRef = useRef(3);
+    const [todos, dispatch] = useReducer(reducer, mockData);
+    const idRef = useRef(3);
 
   const onUpdate = (targetId) =>{
-      //Todos State 값들 중에 targetId 같은거 isDone 바꾸기
-      setTodos(todos.map((todo)=>
-          todo.id === targetId
-              ? { ...todo, isDone : !todo.isDone}
-              : todo
-      )
-      )
+      dispatch({
+          type : "UPDATE",
+          targetId : targetId
+      })
+      // //Todos State 값들 중에 targetId 같은거 isDone 바꾸기
+      // setTodos(todos.map((todo)=>
+      //     todo.id === targetId
+      //         ? { ...todo, isDone : !todo.isDone}
+      //         : todo
+      // )
+      // )
   }
 
   const onDelete = (targetId) =>{
         //Todos State 값들 중에 targetId 같은거 isDone 바꾸기
-        setTodos(todos.filter((todo)=>todo.id !== targetId))
+      dispatch({
+          type : "DELETE",
+          targetId : targetId
+      })
+        // setTodos(todos.filter((todo)=>todo.id !== targetId))
     }
 
   const onCreate = (content) =>{
-      const newTodo = {
-          id : idRef.current++,
-          isDone : false,
-          content : content,
-          date : new Date().getTime()
-      }
-
-      setTodos( [newTodo, ...todos])
+      dispatch({
+          type :"CREATE",
+          data : {
+              id : idRef.current++,
+              isDone: false,
+              content : content,
+              date : new Date().getTime(),
+          },
+      });
+      // const newTodo = {
+      //     id : idRef.current++,
+      //     isDone : false,
+      //     content : content,
+      //     date : new Date().getTime()
+      // }
+      //
+      // setTodos( [newTodo, ...todos])
   }
 
 
